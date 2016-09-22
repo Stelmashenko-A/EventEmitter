176018fe34310a4a648274a9910b622e6eff425a
Stelmashenko-A <stelma_28@bk.ru>
2016-09-22 20:31:58 +0300

    [ASD-1] Initial script

---
+++ Database/Authentication database/[ASD-1] Initial script.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE AuthorizationServer;
+GO
+
+CREATE TABLE AuthorizationServer.dbo.[Application]
+(
+	ApplicationId UNIQUEIDENTIFIER NOT NULL PRIMARY KEY,
+	Name nvarchar(255) NOT NULL,
+	ApplicationBase64Secret nvarchar(255) NOT NULL
+); 
+
+CREATE TABLE AuthorizationServer.dbo.UserAccount
+(
+	UserAccountId UNIQUEIDENTIFIER NOT NULL PRIMARY KEY,
+	Name NVARCHAR(255) NOT NULL,
+	LoginProvider NVARCHAR(255),
+	Base64Secret NVARCHAR(255) NOT NULL,
+	ApplicationId UNIQUEIDENTIFIER NOT NULL,
+	CONSTRAINT fk_UserAccountApplication FOREIGN KEY (ApplicationId)
+	REFERENCES AuthorizationServer.dbo.[Application](ApplicationId)
+); 
