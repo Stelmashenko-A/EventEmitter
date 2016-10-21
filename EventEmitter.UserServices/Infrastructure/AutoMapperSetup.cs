22daf5927e9718cfe3ac5d1f72b7b01d5a45ffc0
Stelmashenko-A <stelma_28@bk.ru>
2016-10-21 22:56:20 +0300

    [S-10] AutoMapper

---
+++ EventEmitter.UserServices/Infrastructure/AutoMapperSetup.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using EventEmitter.UserServices.Models;
+
+namespace EventEmitter.UserServices.Infrastructure
+{
+    public class AutoMapperSetup
+    {
+        public IConfigurationProvider Setup()
+        {
+            return new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Contact, Storage.POCO.Contact>();
+                cfg.CreateMap<Storage.POCO.Contact, Contact>();
+
+                cfg.CreateMap<Event, Storage.POCO.Event>();
+                cfg.CreateMap<Storage.POCO.Event, Event>();
+
+                cfg.CreateMap<Message, Storage.POCO.Message>();
+                cfg.CreateMap<Storage.POCO.Message, Message>();
+
+                cfg.CreateMap<Registration, Storage.POCO.Registration>();
+                cfg.CreateMap<Storage.POCO.Registration, Registration>();
+
+                cfg.CreateMap<UserAccount, Storage.POCO.UserAccount>();
+                cfg.CreateMap<Storage.POCO.UserAccount, UserAccount>();
+
+            });
+        }
+    }
+}
