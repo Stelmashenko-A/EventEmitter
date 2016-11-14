74d5292394c91f6fdb81e8de1289ab44d9374440
Stelmashenko-A <stelma_28@bk.ru>
2016-11-14 22:11:20 +0300

    [C-16] cards

--- EventEmitter.Client/src/components/Event/Event.js
+++ EventEmitter.Client/src/components/Event/Event.js
@@ -1,14 +1,34 @@
 import React from 'react'
 import './Event.scss'
-
+import Box from 'grommet/components/Box'
+import Card from 'grommet/components/Card'
+import Label from 'grommet/components/Label'
+import Heading from 'grommet/components/Heading'
+import Paragraph from 'grommet/components/Paragraph'
+import Image from 'grommet/components/Image'
 export const Event = (props) => (
   <div className=''>
-    <div>{props.owner}</div>
-    <div>{props.start}</div>
-    <div>{props.duration}</div>
-    <div>{props.price}</div>
-    <div>{props.description}</div>
-    <div>{props.slots}</div>
+    <Box colorIndex='light-2' pad='medium'>
+      <Card label={
+        <Label margin='none' uppercase>
+          <div>{props.owner}</div>
+        </Label>
+      } thumbnail={<Image src='/img/carousel-1.png' />} heading={
+        <Heading tag='h2' strong>
+          <div>{props.start}</div>
+        </Heading>
+      } description={
+        <Paragraph size='large' margin='medium'>
+          <div>{props.start}</div>
+          <div>{props.duration}</div>
+          <div>{props.price}</div>
+          <div>{props.slots}</div>
+          <div>{props.description}</div>
+        </Paragraph>
+      } />
+    </Box>
+
+    
 
   </div>
 
