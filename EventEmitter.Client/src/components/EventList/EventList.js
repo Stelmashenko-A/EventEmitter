c25e48a36d2fea9d211356f0cd95d4b39a8619a3
Stelmashenko-A <stelma_28@bk.ru>
2017-01-25 22:48:38 +0300

    fix bug: 2 clicks to enter login page

--- EventEmitter.Client/src/components/EventList/EventList.js
+++ EventEmitter.Client/src/components/EventList/EventList.js
@@ -3,22 +3,29 @@ import './EventList.scss'
 import Event from '../Event/Event'
 import Tiles from 'grommet/components/Tiles'
 import Button from 'grommet/components/Button'
+
+function renderEvent (event, i) {
+  if (event != null) {
+    return <Event
+      key={i}
+      owner={event.Author}
+      start={event.Start}
+      duration={event.Duration}
+      price={event.Price}
+      description={event.Description}
+      slots={event.Slots}
+      name={event.Name}
+      image={event.Image}
+      id={event.Id}
+      />
+  }
+}
+
 export const EventList = (props) => (
   <div className='event-list'>
     <Tiles colorIndex='light-2' justify='center'>
       {(props.eventList.events.map((event, i) =>
-        <Event
-          key={i}
-          owner={event.Author}
-          start={event.Start}
-          duration={event.Duration}
-          price={event.Price}
-          description={event.Description}
-          slots={event.Slots}
-          name={event.Name}
-          image={event.Image}
-          id={event.Id}
-          />
+         renderEvent(event, i)
       ))}
     </Tiles>
     <Button label='Load more' onClick={props.loading} />
