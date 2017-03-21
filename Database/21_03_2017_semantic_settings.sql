276696d943329f7abd1da9184b61f505822d4bc1
Stelmashenko-A <stelma_28@bk.ru>
2017-03-21 21:01:39 +0300

    settings for sentiments

---
+++ Database/21_03_2017_semantic_settings.sql
@@ -0,0 +1,5 @@
+INSERT INTO Settings(SettingId, Name, Value) VALUES ('86D3E3B7-68A9-480B-A0ED-9882231DEBBD' , 'EMOTICONS_SENTIMENTS', 'G:\Sentimenst\EmoticonSentimentLexicon.txt');
+GO
+
+INSERT INTO Settings(SettingId, Name, Value) VALUES ('6FDA6AF2-9AAA-4883-9AB1-14A539C66074' , 'WORDS_SENTIMENTS', 'G:\Sentimenst\sentiments.csv');
+GO
